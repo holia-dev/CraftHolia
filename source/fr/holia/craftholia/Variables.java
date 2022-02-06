897f80635663757786e7069e862ee9b9ec38369c
Holia <90980259+holia-dev@users.noreply.github.com>
2022-02-07 00:58:30 +0100

    Create Variables.java

---
+++ source/fr/holia/craftholia/Variables.java
@@ -0,0 +1,20 @@
+package fr.holia.craftholia;
+
+/*
+ * 
+ * 
+ * CraftHolia Project.
+ * Created by Holia#3033
+ * 
+ * Website of the source :
+ * https://github.com/holia-dev/CraftHolia
+ * 
+ * 
+ */
+
+public class Variables {
+
+	public static String version = "0.1";
+	public static String typeVersion = "official";
+	
+}
